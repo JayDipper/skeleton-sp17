b9c659505ae406db0b4e0ad42a92028b78d2200f
JayDipper <jaydipper@icloud.com>
2017-06-10 17:40:11 +0800

    completed lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,18 @@
+public class HelloNumbers {
+	public static void main(String [] args) {
+		int x = 0;
+        int s = 0;
+		while (x < 10) {
+		   System.out.println(s);
+		   x = x + 1;
+           s = s + x;
+		}
+	}
+}
+
+/*
+ 1.Before Javavariables can be used, they must be declared.
+ 2.Java variables must be a specific type.
+ 3.Java variable types can never change.
+ 4. Types are verified before the code even runs!!!!
+*/
